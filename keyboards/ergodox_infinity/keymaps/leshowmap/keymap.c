3e49f707a4ab9782479c630edda7e545161c4298
Evan Cameron <cameron.evan@gmail.com>
2017-09-20 20:24:02 -0400

    swap scmd with meh

--- keyboards/ergodox_infinity/keymaps/leshowmap/keymap.c
+++ keyboards/ergodox_infinity/keymaps/leshowmap/keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab/Cmd|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| '/ Cmd | // TODO: think about using ; as the L1 toggle
  * |--------+------+------+------+------+------| SCMD |           | SCMD |------+------+------+------+------+--------|
- * | (/LShf |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  | ./Alt| //Ctl| RShft/)|
+ * | (/LShf |Z/Ctrl|   X  |   C  |   V  |   B  | /cap |           | /cap |   N  |   M  |   ,  | ./Alt| //Ctl| RShft/)|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | cap/shft |  Alt | Down |  Up  | Ctrl |                                       |  <-  | Down |  up  |  ->  | SCMD |
+ *   | Meh  |  Alt | Down |  Up  | Ctrl |                                       |  <-  | Down |  up  |  ->  | Meh  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Ins  | Caps |       | AltGr| RCtrl|
@@ -71,16 +71,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             TT(4), KC_1, KC_2, KC_3, KC_4, KC_5, KC_ESCAPE,
             KC_DELETE, KC_Q, KC_W, KC_E, KC_R, KC_T, M(LAUNCH_TERM),
             GUI_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, LT(3, KC_G),
-            KC_LSPO, CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, SCMD_T(KC_NO),
-            SFT_T(KC_CAPSLOCK), KC_LALT, KC_DOWN, KC_UP, KC_LCTL,
+            KC_LSPO, CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B, SCMD_T(KC_CAPSLOCK),
+            MEH_T(KC_NO), KC_LALT, KC_DOWN, KC_UP, KC_LCTL,
             // thumb
             KC_INSERT, KC_CAPSLOCK, KC_HOME, LT(1, KC_BSPACE), LT(2, KC_DELETE), KC_END,
             // right hand
             KC_ESCAPE, KC_6, KC_7, KC_8, KC_9, KC_0, TT(4),
             M(LAUNCH_ROFI), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPACE,
             KC_H, KC_J, KC_K, KC_L, KC_SCOLON, GUI_T(KC_QUOTE),
-            SCMD_T(KC_NO), KC_N, KC_M, KC_COMMA, ALT_T(KC_DOT), CTL_T(KC_SLASH), KC_RSPC,
-            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, SFT_T(KC_CAPSLOCK),
+            SCMD_T(KC_CAPSLOCK), KC_N, KC_M, KC_COMMA, ALT_T(KC_DOT), CTL_T(KC_SLASH), KC_RSPC,
+            KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, MEH_T(KC_NO),
             // thumb
             KC_LALT, KC_RCTRL, KC_PGUP, KC_PGDOWN, SFT_T(KC_ENTER), LT(1, KC_SPACE)
             // // left hand
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             //      KC_PGUP,
             //      KC_PGDN,KC_TAB, KC_ENT
             ),
-        /* Keymap 1: Symbol Layer
+        /* MINE Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   \  |   &  |   ]  |   [  |   -  |------|           |------|   _  |   (  |   )  |   $  |   :  |    "   |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |   ,  |   .  |   `  |   +  |      |           |      |   ~  |   |  |   <  |   >  |   \  |        |
+ * |         |  '   |   ,  |   .  |   `  |   +  |      |           |      |   ~  |   |  |   <  |   >  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |       |      |      |      |      |                                       |      |      |      |      |      |
  *   `-----------------------------------'                                       `----------------------------------'
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F11,
             KC_TRNS, KC_QUES, KC_CIRC, KC_HASH, KC_PERC, KC_ASTR, KC_TRNS,
             KC_TRNS, KC_BSLASH, KC_AMPR, KC_RBRACKET, KC_LBRACKET, KC_MINUS,
-            KC_TRNS, KC_TRNS, KC_COMMA, KC_DOT, KC_GRAVE, KC_PLUS, KC_TRNS,
+            KC_TRNS, KC_QUOT, KC_COMMA, KC_DOT, KC_GRAVE, KC_PLUS, KC_TRNS,
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
             // thumb
             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, // added here
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             //  KC_TRNS,
             //  KC_TRNS, RGB_HUD, RGB_HUI
             ),
-        /* Keymap 2: Media and mouse keys
+        /* MINE Keymap 2: Media and mouse keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Reset  |      |      |      |      |      | NumLk|           |      |      |      |      |      |      | Reset  |
@@ -293,7 +293,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                 //thumb
                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-        /* Keymap 3: Media and mouse keys
+        /* Keymap 4: Media and mouse keys
 *
 * ,--------------------------------------------------.           ,--------------------------------------------------.
 * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
