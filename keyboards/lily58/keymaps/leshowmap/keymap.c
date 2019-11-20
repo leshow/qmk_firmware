056a4cc0c3aeb087f4f20111c5dd5abe8d054a02
Evan Cameron <cameron.evan@gmail.com>
2019-11-20 18:37:37 -0500

    EE_HANDS on lily58

--- keyboards/lily58/keymaps/leshowmap/keymap.c
+++ keyboards/lily58/keymaps/leshowmap/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(MEDIA, KC_DEL), KC_Q, KC_W, KC_E, KC_R, KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   GUI_T(KC_TAB), KC_A, KC_S, KC_D, LT(ARROW,KC_F), KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, GUI_T(KC_QUOT), \
   KC_LSPO, LCTL_T(KC_Z), LALT_T(KC_X), KC_C, LT(MOUSE, KC_V), KC_B, KC_AUDIO_MUTE,    LGUI(KC_X),    KC_N, KC_M, KC_COMM, RALT_T(KC_DOT), RCTL_T(KC_SLSH), KC_RSPC, \
-        LGUI(KC_ENT), KC_LCTL, MO(NUM), LT(SYMB, KC_BSPC),       LT(SYMB, KC_SPC), SGUI_T(KC_ENT),   LT(ADJUST, KC_ESC), KC_TAB \
+        LGUI(KC_ENT), LCTL_T(KC_ESC), MO(NUM), LT(SYMB, KC_BSPC),       LT(SYMB, KC_SPC), SGUI_T(KC_ENT),   LT(ADJUST, KC_ESC), KC_TAB \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -148,15 +148,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 int RGB_current_mode;
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
 
 void matrix_init_user(void) {
     #ifdef RGBLIGHT_ENABLE
